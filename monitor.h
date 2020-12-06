a10315c76440cdce6481cd8a20ad6730364f1ced
i92loden <71695686+i92loden@users.noreply.github.com>
2020-12-06 12:52:59 +0100

    Add files via upload

---
+++ monitor.h
@@ -0,0 +1,20 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+#include <list>
+#include <iostream>
+#include "visita_guiada.h"
+#include <string>
+using namespace std;
+class Monitor{
+private:
+  string nombre_completo_;
+  string DNI_;
+  string usuario_;
+  list <Visita_guiada> visitas_asignadas_;
+public:
+  void getHorario(vector <Visita_guiada> vg);
+  void confirmarAsistencia(Visita_guiada &a);
+  bool identificacion(string dni_,vector <string> v);
+  inline string getDNI(){return DNI_;}
+};
+#endif
