d1a3fd0359fe6cbc9b9eb75c2929193a225f20fd
i92esper <71695797+i92esper@users.noreply.github.com>
2020-12-17 13:10:58 +0100

    Update visita_guiada_unittest.cc
    Agregación de los últimos casos de prueba de la clase visita guiada.

--- Clase Visita Guiada/visita_guiada_unittest.cc
+++ Clase Visita Guiada/visita_guiada_unittest.cc
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-TEST(Visita_guiada, crearVisita){
+TEST(Visita_guiada, crearVisita){ //Creacion correcta de la visita guiada
   Visita_guiada prueba;
   tm fecha;
   fecha.tm_year=2020;
@@ -17,19 +17,19 @@ TEST(Visita_guiada, crearVisita){
   fecha.tm_mday=20;
   string dni="31478961B";
   int ruta=2;
-  string monitor="Nicolás";
+  string monitor="41087678C";
   EXPECT_TRUE(prueba.crearVisita(fecha,dni,ruta,monitor));
   EXPECT_FALSE(prueba.crearVisita(fecha,dni,10,monitor));
 }
 
-TEST(Visita_guiada, setRuta){
+TEST(Visita_guiada, setRuta){ //Ruta valida para la visita
   Visita_guiada prueba;
   EXPECT_FALSE(prueba.setRuta(6));
-  EXPECT_FALSE(prueba.setRUta(0));
+  EXPECT_FALSE(prueba.setRuta(0));
   EXPECT_TRUE(prueba.setRuta(3));
 }
 
-TEST(Visita_guiada, setRealizada){
+TEST(Visita_guiada, setRealizada){ // Visita realizada o cancelada por el visitante correctamente
   Visita_guiada prueba;
   EXPECT_EQ(CREATED,prueba.getRealizada());
   tm fecha;
@@ -38,7 +38,7 @@ TEST(Visita_guiada, setRealizada){
   fecha.tm_mday=20;
   string dni="31478961B";
   int ruta=2;
-  string monitor="Nicolás";
+  string monitor="41087678C";
   prueba.crearVisita(fecha,dni,ruta,monitor);
   EXPECT_EQ(VERIFIED,prueba.getRealizada());
   prueba.setRealizada();
@@ -47,15 +47,39 @@ TEST(Visita_guiada, setRealizada){
   EXPECT_EQ(DELETED,prueba.getRealizada());
 }
 
-TEST(Visita_guiada, modificarVisita){
+TEST(Visita_guiada, Fechavalida){ //Antelacion de dos meses, superior o igual a la actual
   Visita_guiada prueba;
   EXPECT_FALSE(prueba.modificarVisita(10));
   EXPECT_TRUE(prueba.modificarVisita(3));
   tm fecha;
-  fecha.tm_year=2001;
+  fecha.tm_year=1900;
   fecha.tm_mon=11;
   fecha.tm_mday=20;
-  EXPECT_FALSE(prueba.modificarVisita(fecha));
+  EXPECT_FALSE(prueba.modificarVisita(fecha)); //Error a la hora de meter una fecha invalida
   fecha.tm_year=2020;
   EXPECT_TRUE(prueba.modificarVisita(fecha));
 }
+
+TEST(Visita_guiada, visitantesMaximo){ //Numero de visitantes maximo permitido
+  Visita_guiada prueba;
+  EXPECT_FALSE(prueba.setNvisitantes(-5));
+  EXPECT_FALSE(prueba.setNvisitantes(12));
+  EXPECT_TRUE(prueba.setNvisitantes(5));
+  EXPECT_EQ(prueba.getNvisitantes(),5);
+}
+
+TEST(Visita_guiada, creacionID){ //Creacion de un ID asignado a cada visita
+  Visita_guiada prueba;
+  tm fecha;
+  fecha.tm_year=2020;
+  fecha.tm_mon=11;
+  fecha.tm_mday=20;
+  string dni="31478961B";
+  int ruta=2;
+  string monitor="41087678C";
+  prueba.crearVisita(fecha,dni,ruta,monitor);
+  string id;
+  id=prueba.getID();
+  EXPECT_EQ(5,id.size());
+}
+
