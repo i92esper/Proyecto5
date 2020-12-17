31356610f5237c2d6367cd49b81f9295332662d5
i92loden <71695686+i92loden@users.noreply.github.com>
2020-12-17 21:00:31 +0100

    Actualización

--- Clase Incidencia/incidencia.cc
+++ Clase Incidencia/incidencia.cc
@@ -2,32 +2,29 @@
 #include <vector>
 #include "incidencia.h"
 using namespace std;
-Incidencia Incidencia::getIncidencia(){
-Incidencia a(gettipo_incidencia(),getinforme());
-return a;
-}
- Incidencia Incidencia::setIncidencia(){
-  cout<<"Introduce un tipo de error\n1.Cancelacion\n2.no presentado\n3.otro\n"<<endl;
-  int a;
-  do{
-  cin>>a;
-}while(a>3&&a<1);
-  cout<<"\nIntroduce el informe de la incidencia\n"<<endl;
-  string s;
-  cin>>s;
-  Incidencia i(a,s);
-  return i;
+
+    bool Incidencia::setIncidencia(int tipo_incidencia, string informe){
+if(!setInforme(informe)){return false;}
+if(!settipo_incidencia(tipo_incidencia)){return false;}
+return true;
 }
 
 void Incidencia::notificaIncidencia(){
   cout<<"Hay una nueva incidencia";
 }
-void Incidencia::eliminarIncidencia(vector <Incidencia> v,Incidencia i){
-  for(vector <Incidencia>:: iterator it=v.begin();it!=v.end();it++){
-    if(i.informe_.compare(it->getinforme())==0){
-      it->resuelta_==true;
-      v.erase(it);
+void Incidencia::eliminarIncidencia(vector <Incidencia> v){
+for(vector <Incidencia>:: iterator it=v.end();it!=v.begin();it--){
+    if(informe_.compare(it->getinforme())==0){
+      it->setResuelta();
+      v.erase(it);}
     }
   }
-
+bool Incidencia::setInforme(string informe){
+  if(informe==""){return false;}
+    informe_=informe;
+    return true;
+}
+bool Incidencia::settipo_incidencia(int tipo){
+  tipo_incidencia_=tipo;
+  return true;
 }
