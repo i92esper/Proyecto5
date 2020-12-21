2258d79e46c60aa62bd47a103eded6d887ed34c1
i92loden <71695686+i92loden@users.noreply.github.com>
2020-12-21 13:36:50 +0100

    monitor actualizado

---
+++ Clase Monitor 1.1/monitor.cc
@@ -0,0 +1,64 @@
+//Monitor.cc, por Nicolas Lopez
+
+#include <list>
+#include <vector>
+#include "visita_guiada.h"
+#include "monitor.h"
+#include <string>
+#include <iostream>
+#include <ctime>
+using namespace std;
+//definimos el punter ode tm que teendrá la hora local
+tm * lotm;
+Monitor::Monitor(){
+  time_t now=time(NULL);
+  lotm=localtime(&now);
+  nombre_completo_="";
+  DNI_="";
+  estado_=false;
+  fecha_.tm_year=0;
+  fecha_.tm_mon=0;
+  fecha_.tm_mday=0;
+
+  visitas_asignadas_.clear();
+
+}
+bool Monitor::setDNI(string dni){
+  DNI_=dni;
+  return true;
+}
+bool Monitor::identificacion(vector <string> v){
+for (vector<string>::iterator i=v.begin();i!=v.end();i++){
+string a=getDNI(),b=*i;
+if(b.compare(a)==0){
+  return true;}
+}
+return false;
+}
+void Monitor::getHorario(vector <Visita_guiada> vg){
+for (vector<Visita_guiada>::iterator i=vg.begin();i!=vg.end();i++){
+string a=getDNI(),b=i->getMonitor();
+if(b.compare(a)==0 && setFecha(getFecha())){
+    visitas_asignadas_.push_back(*i);}
+  }
+}
+
+void Monitor::confirmarAsistencia(Visita_guiada &a){
+  a.setRealizada();
+}
+bool Monitor::setFecha(tm fecha){
+if(fecha.tm_year < lotm->tm_year){return false;}
+if (fecha.tm_mon < lotm->tm_mon && fecha.tm_year == lotm->tm_year){return false;}
+if(fecha.tm_mon == lotm->tm_mon && fecha.tm_year == lotm->tm_year && fecha.tm_mday < lotm->tm_mday){return false;}
+fecha_=fecha;
+return true;
+}
+bool Monitor::crearMonitor(string nombre,string dni,tm fecha){
+nombre_completo_=nombre;
+if(!setDNI(dni)) {return false;}
+DNI_=dni;
+if(!setFecha(fecha)){return false;}
+fecha_=fecha;
+estado_=true;
+return true;
+};
---
+++ Clase Monitor 1.1/monitor_unittest.cc
@@ -0,0 +1,68 @@
+#include "gtest/gtest.h"
+#include "monitor.h"
+#include "visita_guiada.h"
+#include <ctime>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+//prueba que se recojan las visitas asignadas a ese dni
+TEST(Monitor,getHorario){
+  Monitor test;
+  test.setDNI("31023573T");
+  Visita_guiada a;
+  tm fecha;
+  fecha.tm_year=2020;
+  fecha.tm_mon=12;
+  fecha.tm_mday=17;
+  a.crearVisita(fecha,"31478961B",1,"31023573T");
+  vector <Visita_guiada> v;
+  v.push_back(a);
+  test.getHorario(v);
+  EXPECT_EQ(1,test.getvisitas_asignadas_size());
+}
+//prueba que se cree correctamente el usuario
+TEST(Monitor,estado){
+  Monitor test,test2;
+  tm fecha;
+  fecha.tm_year=2020;
+  fecha.tm_mon=12;
+  fecha.tm_mday=25;
+  test.crearMonitor("31023573T","Alfonso",fecha);
+  EXPECT_TRUE(test.getEstado());
+  EXPECT_FALSE(test2.getEstado());
+
+}
+
+
+//comprueba que se cambie el estado de una visita concreta a done
+TEST(Monitor,confirmarAsistencia){
+Visita_guiada a;
+tm fecha;
+fecha.tm_year=2020;
+fecha.tm_mon=12;
+fecha.tm_mday=17;
+Monitor test;
+test.crearMonitor("31023573T","Alfonso",fecha);
+  a.crearVisita(fecha,"31478961B",1,"31023573T");
+test.confirmarAsistencia(a);
+  EXPECT_EQ(DONE,a.getRealizada());
+
+}
+//comprueba que funcione la identificacion comparando un dni con un vector de dnis
+TEST(Monitor,identificacion){
+  tm fecha;
+  fecha.tm_year=2020;
+  fecha.tm_mon=12;
+  fecha.tm_mday=25;
+  Monitor test;
+  test.crearMonitor("31023573T","Alfonso",fecha);
+  Monitor test2;
+  test2.crearMonitor("31478961B","Jose",fecha);
+  vector <string> a;
+  Monitor test3;
+  a.push_back(test.getDNI());
+  a.push_back(test2.getDNI());
+  EXPECT_TRUE(test.identificacion(a)) ;
+  EXPECT_FALSE(test3.identificacion(a));
+}
