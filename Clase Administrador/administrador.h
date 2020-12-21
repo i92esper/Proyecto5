ddbb42c8895c6d1cfb99bf11615f0dc1b9082643
i92loden <71695686+i92loden@users.noreply.github.com>
2020-12-21 12:53:47 +0100

    Update administrador.h

--- Clase Administrador/administrador.h
+++ Clase Administrador/administrador.h
@@ -1,4 +1,3 @@
-//administrador, por Nicolas Lopez
 #ifndef ADMINISTRADOR_H
 #define ADMINISTRADOR_H
 #include <vector>
@@ -7,24 +6,17 @@
 using namespace std;
 class Administrador{
 private:
-  //nombre del administrador
   string usuario_;
-  //codigo de identificacion
   string codigo_;
 public:
-//constructor básico
-  Administrador(string usuario,string codigo){
+Administrador(string usuario,string codigo){
 usuario_=usuario;
 codigo_=codigo;
 }
-  //inicia la sesion si el nombre está en el vector
 bool iniciarSesion(vector <Administrador> a);
-//termina la ejecucción del programa
-  void cerrarSesion();
-//mete un elemento en el vector
-  int verificarUsuario(vector <Administrador> a);
-//serie de gets y sets
- inline string getUsuario(){return usuario_;};
+void cerrarSesion();
+int verificarUsuario(vector <Administrador> a);
+inline string getUsuario(){return usuario_;};
 inline string getCodigo(){return codigo_;};
 inline void setCodigo(string codigo){codigo_=codigo;};
 inline void setUsuario(string usuario){usuario_=usuario;};
